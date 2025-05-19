4d62a612271f694ed7e7ec7c19761fbe38931b7b
Stiven <stivenpilca@gmail.com>
2025-05-18 21:41:31 -0500

    feat: add 2 new endoints
    - Add endpoint to send emails.
    - Add endoint to create a database in Notion.

--- src/ip/ip.controller.ts
+++ src/ip/ip.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { IpService } from './ip.service';
 
-@Controller('ip')
+@Controller('services')
 export class IpController {
   constructor(private readonly ipService: IpService) {}
 
-  @Get()
+  @Get('ip')
   findAll() {
     return this.ipService.findAll();
+  }
+
+  @Post('notion')
+  createDatabase(@Body() body: any) {
+    return this.ipService.createDatabase(body);
+  }
+
+  @Post('email')
+  sendEmail(@Body() body: any) {
+    return this.ipService.sendEmail(body);
   }
 }
--- src/ip/ip.service.ts
+++ src/ip/ip.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { Client } from '@notionhq/client';
+import { Resend } from 'resend';
 
 @Injectable()
 export class IpService {
@@ -9,5 +11,52 @@ export class IpService {
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return publicIp;
+  }
+
+  async createDatabase(tableName: any) {
+    const notion = new Client({
+      auth: process.env.NOTION_TOKEN,
+    });
+    const pageID = process.env.PAGE_ID;
+    try {
+      const newDb = await notion.databases.create({
+        parent: {
+          type: 'page_id',
+          page_id: pageID || '',
+        },
+        title: [
+          {
+            type: 'text',
+            text: {
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+              content: tableName.title,
+            },
+          },
+        ],
+        properties: {
+          Nombre: {
+            title: {},
+          },
+          Email: {
+            rich_text: {},
+          },
+        },
+      });
+      return newDb;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async sendEmail(body: any) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    await resend.emails.send({
+      from: 'Distribuida <onboarding@resend.dev>',
+      to: ['stivenpilca@gmail.com'],
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      subject: body.subject,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      html: `<p>${body.content}</p>`,
+    });
   }
 }
